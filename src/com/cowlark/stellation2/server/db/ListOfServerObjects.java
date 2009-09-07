94a52ac2f0f9c131505d570cbc7ea2a090cd3cb0
dtrg <dtrg>
2009-09-07 21:48:40 +0000

    Cleanup.

--- src/com/cowlark/stellation2/server/db/ListOfServerObjects.java
+++ src/com/cowlark/stellation2/server/db/ListOfServerObjects.java
@@ -1,19 +1,14 @@
 /* An ordered set of server-side database objects.
  * $Source: /cvsroot/stellation/stellation2/src/com/cowlark/stellation2/server/db/ListOfServerObjects.java,v $
- * $Date: 2009/09/06 17:59:15 $
+ * $Date: 2009/09/07 21:48:40 $
  * $Author: dtrg $
- * $Revision: 1.1 $
+ * $Revision: 1.2 $
  */
 
 package com.cowlark.stellation2.server.db;
 
 import java.io.Serializable;
-import java.util.Collection;
-import java.util.Comparator;
 import java.util.LinkedList;
-import java.util.List;
-import java.util.ListIterator;
-import java.util.SortedSet;
 import com.cowlark.stellation2.common.db.HasDBRepresentation;
 import com.cowlark.stellation2.common.db.ListOfClientObjects;
 import com.cowlark.stellation2.common.db.ListOfObjects;
