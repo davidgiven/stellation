bb73e1078b1556bb65493a3164825995f55bab08
dtrg <dtrg>
2009-09-16 23:13:12 +0000

    Add star-level logging (goes to all players in that system).

--- src/com/cowlark/stellation2/server/model/SFleet.java
+++ src/com/cowlark/stellation2/server/model/SFleet.java
@@ -1,8 +1,8 @@
 /* Server-side fleet.
  * $Source: /cvsroot/stellation/stellation2/src/com/cowlark/stellation2/server/model/SFleet.java,v $
- * $Date: 2009/09/14 22:15:34 $
+ * $Date: 2009/09/16 23:13:12 $
  * $Author: dtrg $
- * $Revision: 1.4 $
+ * $Revision: 1.5 $
  */
 
 package com.cowlark.stellation2.server.model;
@@ -119,21 +119,8 @@ public class SFleet extends SObject
 	public void checkObjectVisibleTo(SPlayer player)
 		throws StellationException
 	{
-		/* If any fleet in this system is owned by the player, and that fleet
-		 * has a jumpship, the fleet is visible. */
+		/* Delegate to Star. */
 		
-		for (SObject o : getLocation())
-		{
-			SFleet fleet = o.toFleet();
-			if ((fleet != null) && (fleet.getOwner() == player) &&
-				(fleet.getJumpshipCount() > 0))
-			{
-				return;
-			}
-		}
-		
-		/* The object isn't visible! */
-		
-		throw new InvalidObjectException(getId());
+		getLocation().checkObjectVisibleTo(player);
 	}
 }
--- src/com/cowlark/stellation2/server/model/SStar.java
+++ src/com/cowlark/stellation2/server/model/SStar.java
@@ -1,16 +1,19 @@
 /* Server-side star.
  * $Source: /cvsroot/stellation/stellation2/src/com/cowlark/stellation2/server/model/SStar.java,v $
- * $Date: 2009/09/06 17:59:15 $
+ * $Date: 2009/09/16 23:13:12 $
  * $Author: dtrg $
- * $Revision: 1.1 $
+ * $Revision: 1.2 $
  */
 
 package com.cowlark.stellation2.server.model;
 
+import java.util.HashSet;
 import com.cowlark.stellation2.common.Asteroids;
 import com.cowlark.stellation2.common.Resources;
 import com.cowlark.stellation2.common.S;
+import com.cowlark.stellation2.common.exceptions.InvalidObjectException;
 import com.cowlark.stellation2.common.exceptions.ResourcesNotAvailableException;
+import com.cowlark.stellation2.common.exceptions.StellationException;
 import com.cowlark.stellation2.common.model.CStar;
 import com.cowlark.stellation2.server.db.CClass;
 import com.cowlark.stellation2.server.db.Property;
@@ -98,4 +101,46 @@ public class SStar extends SObject
 	{
 		destroyResources(r);
 	}
+	
+	public void checkObjectVisibleTo(SPlayer player)
+		throws StellationException
+	{
+		/* If any fleet in this system is owned by the player, and that fleet
+		 * has a jumpship, the fleet is visible. */
+		
+		for (SObject o : this)
+		{
+			SFleet fleet = o.toFleet();
+			if ((fleet != null) && (fleet.getOwner() == player) &&
+				(fleet.getJumpshipCount() > 0))
+			{
+				return;
+			}
+		}
+		
+		/* The object isn't visible! */
+		
+		throw new InvalidObjectException(getId());
+	}
+	
+    public SStar log(String message)
+    {
+    	/* Send to all players with a presence in this system. */
+    	
+    	HashSet<SPlayer> players = new HashSet<SPlayer>();
+    	for (SObject o : this)
+    	{
+			SFleet fleet = o.toFleet();
+			if ((fleet != null) &&
+				(fleet.getJumpshipCount() > 0))
+			{
+				players.add(fleet.getOwner());
+			}
+    	}
+    	
+    	for (SPlayer player : players)
+    		player.log(message);
+    	
+    	return this;
+    }	
 }
