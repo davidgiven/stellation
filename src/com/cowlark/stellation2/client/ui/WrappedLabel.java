0e1dfd0ab6ff95d73b4b92a94e15f29bbdd97fe7
dtrg <dtrg>
2009-09-20 22:12:52 +0000

    Don't use InlineLabel for WrappedLabel (because most formatting doesn't work on it).

--- src/com/cowlark/stellation2/client/ui/WrappedLabel.java
+++ src/com/cowlark/stellation2/client/ui/WrappedLabel.java
@@ -1,20 +1,20 @@
 /* A word-wrapped label.
  * $Source: /cvsroot/stellation/stellation2/src/com/cowlark/stellation2/client/ui/WrappedLabel.java,v $
- * $Date: 2009/09/07 21:48:10 $
+ * $Date: 2009/09/20 22:12:52 $
  * $Author: dtrg $
- * $Revision: 1.3 $
+ * $Revision: 1.4 $
  */
 
 package com.cowlark.stellation2.client.ui;
 
-import com.google.gwt.user.client.ui.InlineLabel;
+import com.google.gwt.user.client.ui.Label;
 
-public class WrappedLabel extends InlineLabel
+public class WrappedLabel extends Label
 {
 	public WrappedLabel(String s)
     {
-		super(s);
-		setStylePrimaryName("Text");
+		super(s, true);
+		addStyleName("Text");
     }
 	
 	public WrappedLabel()
