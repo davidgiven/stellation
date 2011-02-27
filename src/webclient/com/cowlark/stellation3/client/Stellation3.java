c895f4d9fb5b34eed334020a21b4c94e370e89f6
David Given <dg@cowlark.com>
2011-02-27 19:10:10 +0000

    Added skeleton for web front end.

---
+++ src/webclient/com/cowlark/stellation3/client/Stellation3.java
@@ -0,0 +1,11 @@
+
+package com.cowlark.stellation3.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class Stellation3 implements EntryPoint
+{
+	public void onModuleLoad()
+	{
+	}
+}
