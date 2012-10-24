acd1ef482a44033e073fdb977a02cb1253f7efef
David Given <dg@cowlark.com>
2012-10-24 23:02:35 +0000

    Web client will now ask for login details and authenticate.

--- src/client/scripts/main.js
+++ src/client/scripts/main.js
@@ -1,35 +1,12 @@
 "use strict";
 
 define(
-	["jquery", "IO", "Database"],
-	function ($, IO, Database)
+	["jquery", "GameStateMachine"],
+	function ($, GameStateMachine)
 	{
 		return {
-			main: function()
-			{
-				Database.Reset();
-				IO.GetStatics(
-					function ()
-					{
-        				IO.RPC(
-        					{
-        						cmd: "Authenticate",
-        						email: "test@invalid.com",
-        						password: "password"
-        					},
-        					function (msg)
-        					{
-        						if (msg.result === "OK")
-        						{
-        							IO.SetCookie(msg.cookie);
-        							IO.GameCommand({});
-        						}
-        					}
-        				);
-					}
-				);
-			}
-		};
+			main: GameStateMachine.Start
+		}; 
 	}
 );
 
