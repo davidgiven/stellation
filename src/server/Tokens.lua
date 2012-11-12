a2e44061ee78c880f163119737734607b5534111
David Given <dg@cowlark.com>
2012-11-12 23:13:21 +0000

    Fix the token map so that caching, like, works.

--- src/server/Tokens.lua
+++ src/server/Tokens.lua
@@ -3,54 +3,58 @@ local Log = require("Log")
 local Database = require("Database")
 local SQL = Database.SQL
 
-local tokenMap
+local tokenMap = {}
 
-local function reset()
-	Log.D("flushing token cache")
-	tokenMap = {}
-	setmetatable(tokenMap,
-	{
-		__index = function (self, v)
-			if (type(v) == "number") then
-				-- number -> token
-				
-				local value = v
-				local row = SQL(
-					"SELECT value FROM tokens WHERE id = ?"
-				):bind(value):step()
-				
-				local token = row[1]
-				Log.D("token '", token, "' has id ", value)
-				rawset(tokenMap, token, value)
-				rawset(tokenMap, value, token)
-				
-				return token
-			else
-				-- token -> number
+local function number_to_token(number)
+	local row = SQL(
+		"SELECT value FROM tokens WHERE id = ?"
+	):bind(number):step()
 	
-				local token = v
-				Utils.Assert(not tonumber(token), "tokens should be alphanumeric!")
-							
-				SQL(
-					"INSERT OR IGNORE INTO tokens VALUES (NULL, ?)"
-				):bind(token):step()
-					
-				local row = SQL(
-					"SELECT id FROM tokens WHERE value = ?"
-				):bind(token):step()
-				
-				local value = tonumber(row[1])
-				Log.D("token '", token, "' has id ", value)
-				rawset(tokenMap, token, value)
-				rawset(tokenMap, value, token)
-				
-				return value
-			end
-		end
-	})
+	local token = row[1]
+	Log.D("token ", number, " -> '", token, "'")
+	tokenMap[token] = number
+	tokenMap[number] = token
+	
+	return token
 end
 
-reset()
-Database.AddRollbackNotification(reset)
+local function token_to_number(token)
+	Utils.Assert(not tonumber(token), "tokens should be alphanumeric!")
+				
+	SQL(
+		"INSERT OR IGNORE INTO tokens VALUES (NULL, ?)"
+	):bind(token):step()
+		
+	local row = SQL(
+		"SELECT id FROM tokens WHERE value = ?"
+	):bind(token):step()
+	
+	local number = tonumber(row[1])
+	Log.D("token '", token, "' -> ", number)
+	tokenMap[token] = number
+	tokenMap[number] = token
+	
+	return number
+end
+
+setmetatable(tokenMap,
+{
+	__index = function (self, v)
+		if (type(v) == "number") then
+			return number_to_token(v)
+		else
+			return token_to_number(v)
+		end
+	end
+})
+
+Database.AddRollbackNotification(
+	function()
+		Log.D("flushing token cache")
+		for k in pairs(tokenMap) do
+			tokenMap[k] = nil
+		end
+	end
+)
 
 return tokenMap
