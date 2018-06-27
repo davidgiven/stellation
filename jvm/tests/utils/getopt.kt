5bdc2013da43b784262e477a96c2a4551114b61e
David Given <dg@cowlark.com>
2018-06-28 01:20:48 +0200

    Add the boilerplate for junit/mockito tests.

---
+++ jvm/tests/utils/getopt.kt
@@ -0,0 +1,11 @@
+package utils
+
+import com.google.common.truth.Truth.assertThat
+import kotlin.test.Test
+
+class GetoptTest {
+    @Test
+    fun dummyTest() {
+        assertThat(false).isEqualTo(false)
+    }
+}
