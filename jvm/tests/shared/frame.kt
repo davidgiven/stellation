e9ff1f86440f34a7b43943d2114264206dec3bec
David Given <dg@cowlark.com>
2018-07-02 23:24:22 +0200

    Add a basic test for frame serialisation.

---
+++ jvm/tests/shared/frame.kt
@@ -0,0 +1,29 @@
+package shared
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.Before
+import org.junit.Test
+
+class FrameTest {
+    private val WIDTH = 5
+    private val HEIGHT = 4
+    private val DATA = listOf(
+            "00000",
+            "01111",
+            "01221",
+            "01111").joinToString("")
+    private val REP = "$WIDTH#$HEIGHT#$DATA"
+
+    @Before
+    fun before() {
+        assertThat(DATA).hasLength(WIDTH * HEIGHT)
+    }
+
+    @Test
+    fun createFrame() {
+        val frame = Frame(REP)
+        assertThat(frame.width).isEqualTo(WIDTH)
+        assertThat(frame.height).isEqualTo(HEIGHT)
+        assertThat(frame.serialise()).isEqualTo(REP)
+    }
+}
