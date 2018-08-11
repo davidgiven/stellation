3451a3263aba3c7e98b881ba7da816647fd95a20
David Given <dg@cowlark.com>
2018-08-11 19:23:42 +0200

    Add simple zooming; it's a bit slow.

--- src/client/ui/galaxy.kt
+++ src/client/ui/galaxy.kt
@@ -2,8 +2,12 @@ package client.ui
 
 import model.SGalaxy
 import model.SStar
+import org.w3c.dom.events.WheelEvent
 import kotlin.browser.document
 import kotlin.browser.window
+import kotlin.math.pow
+
+private const val SCALE_STEP = 1.005
 
 class Galaxy(val thing: SGalaxy) {
     private val SIZE = SGalaxy.RADIUS * 1.1
@@ -11,12 +15,15 @@ class Galaxy(val thing: SGalaxy) {
     private val root = SVGElement("svg")
     private var width = 0.0
     private var height = 0.0
+    private var panX = 0.0
+    private var panY = 0.0
     private var size = 0.0
     private var scale = 0.0
 
     init {
         root.classes = setOf("map")
         window.addEventListener("resize", { render() })
+        window.addEventListener("wheel", { onWheelEvent(it as WheelEvent) })
     }
 
     fun attach() {
@@ -65,6 +72,14 @@ class Galaxy(val thing: SGalaxy) {
                 t["transform"] = "translate($sx,$ty)"
                 t.addTo(mapGroup)
             }
+        }
+    }
+
+    fun onWheelEvent(event: WheelEvent) {
+        val scaleDelta = SCALE_STEP.pow(-event.deltaY)
+        if (scaleDelta != 0.0) {
+            scale *= scaleDelta
+            render()
         }
     }
 }
